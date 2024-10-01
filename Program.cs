a01106ddd71a495367bc6d3eae506885f2a2301e
fernandastocler <nandasastocler@gmail.com>
2024-10-01 16:05:33 -0300

    Atividade concluida

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Para Meu Curriculo Atividade
+
+O que foi utilizado?
+- .NET 
+- c#
+
+Etapas implementadas
+1. Criação do repositório no GitHub.
+2. Criação de uma aplicação console usando .NET
+3. Implementação de funcionalidades para exibir informações do currículo.
+
+Backlog
+- Melhorar a apresentação do currículo.
+- Adicionar mais informações sobre experiências profissionais.
+- Implementar a funcionalidade de exportação para PDF.
+
+Conclusão
+A aplicação permite uma maneira simples de exibir informações do currículo em um formato de console. Futuras melhorias podem incluir exportação e uma interface gráfica.
