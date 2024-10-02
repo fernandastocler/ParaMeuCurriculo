c69ee9f3301d18375d99f745b7ca4f6b8a79a959
fernandastocler <nandasastocler@gmail.com>
2024-10-02 14:31:20 -0300

    Adicionando atividade

--- obj/Debug/net8.0/ParaMeuCurriculo.AssemblyInfo.cs
+++ obj/Debug/net8.0/ParaMeuCurriculo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParaMeuCurriculo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feb8cb50f32c743eb6eab52f05e1d6a2d0b43a53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50209a7e45efbcc4406a5b7d305e1905e1747ae1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParaMeuCurriculo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParaMeuCurriculo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
